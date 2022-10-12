a1e9c091bf513c3065766fd1ea6c1d5a796b3c30
Admin <andrian.paereli@mail.ru>
2022-10-12 23:24:40 +0300

    structura mapelor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import React from 'react';
-import Header from './components/header';
-import Nav from './components/nav';
-import Home from './components/home';
+import Header from './components/header/header';
+import Nav from './components/nav/nav';
+import Profile from './components/prolfile/profile';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
-      <Home />
+      <Profile />
     </div>
   );
 }
--- src/components/home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import h from './home.module.css';
-const Home = () => {
-    return (
-        <div className={h.home}>Hello. Welcome to my page. It was created to apply the knowledge gained while studying React!</div>
-    );
-}
-export default Home;
